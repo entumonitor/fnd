6fa6507c3afaa0e2d8d664ff50358e5d8ad7baf8
fnd <entumonitor@gmail.com>
2019-09-12 19:11:40 -0300

    prueba de error

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-var http = require('http'),
-    fs = require('fs');
+var http = require('http');
 
-
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
-});
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
